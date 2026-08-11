703bed603c9b5ba645244b539ace7a22e393b1ef
Isaacrypto25 <vinivallettk@gmail.com>
2026-08-11 17:30:39 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -114,6 +114,7 @@ app.use(helmet({
     directives: {
       defaultSrc: ["'self'"],
       scriptSrc: ["'self'", "'unsafe-inline'", "https://cdnjs.cloudflare.com"],
+      scriptSrcAttr: ["'unsafe-inline'"],
       styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
       fontSrc: ["'self'", "https://fonts.gstatic.com", "data:"],
       imgSrc: ["'self'", "data:", "blob:"],
